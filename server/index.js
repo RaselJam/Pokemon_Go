d91adb1314e926d69b46375b7f5f9da746f9d79b
RaselJam <Rasel.jam.h@gmail.com>
2021-11-22 17:24:49 +0100

    Added user related Endpoints and Controler logic

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes.js'
+import session from 'express-session';
 
 const app = express();
 
@@ -12,6 +13,23 @@ dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+// required for the app when deployed to Heroku (in production)
+app.set('trust proxy', 1)
+app.use(
+  session({
+    secret: process.env.SESS_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    cookie: {
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
+      maxAge: 60000*60 // 60 * 1000 ms === 1 min *60 : 1h
+    }
+  })
+
+)
+
 
 app.use('/users', userRoutes);
 app.get('/', (req, res) => { res.send('Hello to Memories API'); });  //const CON
