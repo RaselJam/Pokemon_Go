a671a61d1eca7479b53c739b77f375de0d5be6fb
RaselJam <Rasel.jam.h@gmail.com>
2021-11-24 13:21:24 +0100

    _

--- server/controllers/pokemon.controller.js
+++ server/controllers/pokemon.controller.js
@@ -25,6 +25,26 @@ export const renderSinglePokemonView = (req, res) => {
       res.render('POKEMONS/pokemon-details', pokemon)
     })
     .catch(err => console.log("internal Error 500" + err))
+}
+export const renderEditPokemonView = (req, res) => {
+  const pokemonId = req.params.id;
+  console.log(pokemonId);
+  PokemonModel.findById(pokemonId)
+    .then(pokemon => {
+      res.render('POKEMONS/pokemon-details', pokemon)
+    })
+    .catch(err => console.log("internal server  Error 500" + err))
+
+}
+export const updatePokemon = (req, res) => {
+  const { pokemonId, name, description, attack, life, imageURL, lat, long } = req.body;
+  console.log("creating Pokemon..... : ", { pokemonId, name, description, attack, life, imageURL, lat, long });
+  let location = { type: 'Point', coordinates: [+lat, +long] }
+  PokemonModel.findByIdAndUpdate(pokemonId, { name, description, attack, life, imageURL, location }, { new: true })
+    .then(pokemon => {
+      res.render('POKEMONS/pokemon-edit', pokemon)
+    })
+    .catch(err => console.log("internal server  Error 500" + err))
 
 
 
--- server/routes/pokemon.routes.js
+++ server/routes/pokemon.routes.js
@@ -3,9 +3,11 @@ import * as pokemonLogic from '../controllers/pokemon.controller.js';
 
 const router = express.Router();
 
-router.get('/create',pokemonLogic.renderCreatePokemonView)
+router.get('/create', pokemonLogic.renderCreatePokemonView)
 router.post('/create', pokemonLogic.createPokemon);
- router.get('/:id',pokemonLogic.renderSinglePokemonView)
+router.get('/:id', pokemonLogic.renderSinglePokemonView)
+router.get('/:id', pokemonLogic.renderEditPokemonView)
+ router.post('/update', pokemonLogic.updatePokemon)
 
 
 export default router;
